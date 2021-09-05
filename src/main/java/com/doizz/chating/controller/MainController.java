dbbb3bd1e76df6d2ea12d1d44a32b4b6198b7eb9
home-PC\home <doizz@naver.com>
2021-09-05 19:51:25 +0900

    스프링부트 웹소켓 채팅 리뷰

--- src/main/java/com/doizz/chating/controller/MainController.java
+++ src/main/java/com/doizz/chating/controller/MainController.java
@@ -45,7 +45,9 @@ public class MainController {
 	 */
 	@RequestMapping("/createRoom")
 	public @ResponseBody List<Room> createRoom(@RequestParam HashMap<Object, Object> params){
+		//roomName을 가져온다.
 		String roomName = (String) params.get("roomName");
+		//roomName이 널이 아닐경우 방번호를 생성하여 리턴시킨다.
 		if(roomName != null && !roomName.trim().equals("")) {
 			Room room = new Room();
 			room.setRoomNumber(++roomNumber);
@@ -72,14 +74,17 @@ public class MainController {
 	@RequestMapping("/moveChating")
 	public ModelAndView chating(@RequestParam HashMap<Object, Object> params) {
 		ModelAndView mv = new ModelAndView();
+		//파라미터 넘어온 roomNumber를 변수에 대입한다.
 		int roomNumber = Integer.parseInt((String) params.get("roomNumber"));
-		
+		//roomnumber를 기존에 저장되어있는 roomnumber session에서 찾는다.
 		List<Room> new_list = roomList.stream().filter(o->o.getRoomNumber()==roomNumber).collect(Collectors.toList());
+		//roomnumber가 존재할경우 채팅으로 이동한다.
 		if(new_list != null && new_list.size() > 0) {
 			mv.addObject("roomName", params.get("roomName"));
 			mv.addObject("roomNumber", params.get("roomNumber"));
 			mv.setViewName("chat");
 		}else {
+		//없을경우 방생성 화면으로 이동한다.	
 			mv.setViewName("room");
 		}
 		return mv;
